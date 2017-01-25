976947691302ff1d168a9e1b21b829cbead59417
Viktor Kobryn <spaun1002@gmail.com>
2017-01-25 15:00:56 +0200

    fix bug with instances

--- app/callbacks_events.py
+++ app/callbacks_events.py
@@ -5,16 +5,10 @@ import utils
 from app import constants
 import logging
 from . import pc_controller
+from .metaclasses import Singleton
 
 
-class Callback(pc_controller.PcController):
-    instance = None
-
-    def __new__(cls, *args, **kwargs):
-        # set as singleton
-        if not cls.instance:
-            cls.instance = object.__new__(cls)
-        return cls.instance
+class Callback(pc_controller.PcController, metaclass=Singleton):
 
     def __init__(self, window):
         super(Callback, self).__init__(window.os_version)
@@ -244,8 +238,8 @@ class Callback(pc_controller.PcController):
             self.widget.hide()
 
     def action_ontimer_timeout(self, event, action):
-        print(action)
-        print('what are fuck')
+        logging.debug('Timeout timer. %s' %
+                      constants.PC_ACTION_IN_PROGRESS_TEXT[action])
         if event == constants.DATE_AFTER:
             self.set_disabled_timer(constants.DATE_AFTER, False)
         elif event == constants.DATE_AT:
